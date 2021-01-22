6d0a0ec5471fbc485860111072729b3bca9caa82
ronreiter <ron.reiter@gmail.com>
2021-01-22 03:38:17 +0200

    initial commit

---
+++ bruteforce.py
@@ -0,0 +1,39 @@
+# https://github.com/mcdallas/cryptotools
+
+import sys
+from bip_utils import Bip39MnemonicValidator
+from cryptotools.BTC import Xprv
+import argparse
+
+wordlist = open("bip39_wordslist_en.txt").read().strip().split()
+
+
+def find_correct_seed(words, address_to_find, address_type):
+	for i in range(24):
+		for word_to_try in wordlist:
+			test_array = words.split()
+			test_array[i] = word_to_try
+			test = " ".join(test_array)
+			if not Bip39MnemonicValidator(test).Validate():
+				continue
+			m = Xprv.from_mnemonic(test)
+			m.encode()
+
+			if address_type == "segwit":
+				address = (m/84./0./0./0/0).address('P2WPKH')
+			elif address_type == "legacy":
+				address = (m/44./0./0./0/0).address('P2PKH')
+			else:
+				raise Exception('unknown address type')
+			print(i, word_to_try, address)
+			if address == address_to_find:
+				return test
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--seed", required=True)
+	parser.add_argument("--address", required=True)
+	parser.add_argument("--address-type", choices=["segwit", "default"], default="segwit")
+	args = parser.parse_args()
+
+	print(find_correct_seed(args.seed, args.address, args.address_type))
---
+++ test_bruteforce.py
@@ -0,0 +1,15 @@
+from bruteforce import find_correct_seed
+
+
+#address_to_find = "bc1qf6ypdfhx0wwcxydj09e6ns9r8eqvwpwa4hy7cc"
+# test based on:
+# seed: "account peanut ghost bless crucial enact horse source spread gentle floor write cook fall rail inhale strong lounge cliff play glow pipe symptom enjoy"
+# segwit address: "bc1qcrz2vwkdkzqedzvsdm9fswh3l3cua30580tapj"
+
+address_to_find = "bc1qcrz2vwkdkzqedzvsdm9fswh3l3cua30580tapj"
+original = "account peanut ghost bless crucial enact horse source spread gentle floor write cook fall rail inhale strong lounge cliff play glow pipe symptom enjoy"
+words = "account account ghost bless crucial enact horse source spread gentle floor write cook fall rail inhale strong lounge cliff play glow pipe symptom enjoy"
+
+correct_seed = find_correct_seed(words, address_to_find, "segwit")
+print(correct_seed)
+print(correct_seed == original)
